7d21d67db41d6a34cf7e950994c0bdf95796ca3c
Crash <hex22a@gmail.com>
2023-05-05 18:37:29 +0400

    userdata

--- servers.tf
+++ servers.tf
@@ -15,9 +15,21 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public
+  associate_public_ip_address = true
+
+  user_data = <<EOF
+{
+apt update \
+    && apt install -y ec2-instance-connect ca-certificates wget net-tools gnupg;
+
+wget https://as-repository.openvpn.net/as-repo-public.asc -qO /etc/apt/trusted.gpg.d/as-repository.asc
+echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/as-repository.asc] http://as-repository.openvpn.net/as/debian jammy main">/etc/apt/sources.list.d/openvpn-as-repo.list
+
+apt update && apt -y install openvpn-as
+EOF
 
   tags = {
     Name = "VPN"
