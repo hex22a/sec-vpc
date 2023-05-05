e803390e58c808fe72bf3d420fa517f4f7605c52
Crash <hex22a@gmail.com>
2023-05-05 18:14:08 +0400

    add servers

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ terraform {
       "Name" = "Internet gateway"
     }
 
-    resource "aws_subnet" "public_1" {
+    resource "aws_subnet" "public" {
       vpc_id                  = aws_vpc.sec_vpc.id
       cidr_block              = "42.0.0.0/24"
       map_public_ip_on_launch = true
@@ -40,31 +40,6 @@ terraform {
       }
     }
 
-    resource "aws_subnet" "public_2" {
-      vpc_id                  = aws_vpc.sec_vpc.id
-      cidr_block              = "42.0.1.0/24"
-      map_public_ip_on_launch = true
-      availability_zone       = "us-east-1b"
-
-      tags = {
-        "Name" = "Public subnet 2"
-      }
-    }
-
-    resource "aws_nat_gateway" "nat_gateway" {
-      subnet_id         = aws_subnet.public_1.id
-      allocation_id     = aws_eip.nat_ip.id
-      connectivity_type = "public"
-
-      tags = {
-        "Name" = "Nat gateway"
-      }
-    }
-
-    resource "aws_eip" "nat_ip" {
-      vpc = true
-    }
-
     resource "aws_route_table" "routing_table_public" {
       vpc_id = aws_vpc.sec_vpc.id
 
@@ -73,56 +48,9 @@ terraform {
         gateway_id = aws_internet_gateway.igw.id
       }
     }
-
-    resource "aws_route_table" "routing_table_private" {
-      vpc_id = aws_vpc.sec_vpc.id
-
-      route {
-        cidr_block     = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.nat_gateway.id
-      }
-    }
-
-    resource "aws_subnet" "private_1" {
-      vpc_id                  = aws_vpc.sec_vpc.id
-      cidr_block              = "42.0.2.0/24"
-      map_public_ip_on_launch = false
-      availability_zone       = "us-east-1a"
-
-      tags = {
-        "Name" = "Subnet private 1"
-      }
-    }
-
-    resource "aws_subnet" "private_2" {
-      vpc_id                  = aws_vpc.sec_vpc.id
-      cidr_block              = "42.0.3.0/24"
-      map_public_ip_on_launch = false
-      availability_zone       = "us-east-1b"
-
-      tags = {
-        "Name" = "Subnet private 2"
-      }
-    }
-
-    resource "aws_route_table_association" "rtba_public_1" {
-      subnet_id      = aws_subnet.public_1.id
+    resource "aws_route_table_association" "rtba_public" {
+      subnet_id      = aws_subnet.public.id
       route_table_id = aws_route_table.routing_table_public.id
-    }
-
-    resource "aws_route_table_association" "rtba_public_2" {
-      subnet_id      = aws_subnet.public_2.id
-      route_table_id = aws_route_table.routing_table_public.id
-    }
-
-    resource "aws_route_table_association" "rtba_private_1" {
-      subnet_id      = aws_subnet.private_1.id
-      route_table_id = aws_route_table.routing_table_private.id
-    }
-
-    resource "aws_route_table_association" "rtba_private_2" {
-      subnet_id      = aws_subnet.private_2.id
-      route_table_id = aws_route_table.routing_table_private.id
     }
 
     resource "aws_security_group" "load_balancer_sg" {
@@ -141,49 +69,6 @@ terraform {
         to_port     = 0
         protocol    = "-1"
       }
-    }
-
-    resource "aws_ecs_cluster" "ge_cluster" {
-      name = "ge_cluster"
-
-      setting {
-        name  = "containerInsights"
-        value = "disabled"
-      }
-    }
-
-    resource "aws_ecs_cluster_capacity_providers" "ge_cluster" {
-      cluster_name = aws_ecs_cluster.ge_cluster.name
-
-      capacity_providers = ["FARGATE"]
-
-      default_capacity_provider_strategy {
-        capacity_provider = "FARGATE"
-      }
-    }
-
-    resource "aws_iam_role" "ecs_task_execution_role" {
-      name               = "task_role"
-      assume_role_policy = <<EOF
-{
-  "Version": "2008-10-17",
-  "Statement": [
-      {
-          "Sid": "",
-          "Effect": "Allow",
-          "Principal": {
-              "Service": "ecs-tasks.amazonaws.com"
-          },
-          "Action": "sts:AssumeRole"
-      }
-  ]
-}
-EOF
-    }
-
-    resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
-      role       = aws_iam_role.ecs_task_execution_role.name
-      policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
     }
   }
 }
